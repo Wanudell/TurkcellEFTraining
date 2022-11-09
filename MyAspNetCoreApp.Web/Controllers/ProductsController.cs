5ce490ef9ac76f2e0f043a698ef2edefc341cf24
Berk Ozerdogan <97380286+Wanudell@users.noreply.github.com>
2022-11-09 21:01:55 +0300

    Add project files.

---
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AppDbContext _dbContext;
+
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController(AppDbContext dbContext)
+        {
+            _productRepository = new ProductRepository(); //Artık new keyword'unu kullanmaya gerek yok. Biz aşağıda dbContext'i EF Core sayesinde Dependency Injection yaptık.Yani
+            // Bağımlılıkların yönetilmesini sağlayan bir design pattern'dır Dependency Injection. Bu class'ları almamıza imkan sağlayanlara da DI Container deniyor.
+            // Herhangi bir class'ın ihtiyacı duyduğu nesneyi eğer constructor'dan alıyor ise Dependency Injection design pattern aracılığı ile almış olur.
+            // Bu şekilde nesne almamızı sağlayan yapıya da DI Container diyoruz.
+            _dbContext = dbContext;
+
+            if (!_dbContext.Products.Any()) //Eğer db'de hiç veri yok ise çalışması için yazıldı.
+            {
+                _dbContext.Products.Add(new Product() { Name = "Kalem 1", Price = 100, Stock = 100, Color = "Red", Width = 10, Height = 20 });
+                _dbContext.Products.Add(new Product() { Name = "Kalem 2", Price = 90, Stock = 120, Color = "Blue", Width = 20, Height = 35 });
+                _dbContext.Products.Add(new Product() { Name = "Kalem 3", Price = 80, Stock = 130, Color = "Yellow", Width = 15, Height = 25 });
+
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            //var products = _productRepository.GetAll(); // Biz artık DbContext kullandığımız için artık buna ihtiyaç kalmadı.
+            var products = _dbContext.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = _dbContext.Products.Find(id);
+            //_productRepository.Remove(id); //Artık EntityFramework core kullandığımız için direkt dbContext üzerinden siliyoruz. Eskiden ProductRepository'de oluşturduğumuz methodla siliyorduk.
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAspNetCoreApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyAspNetCoreApp.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //Bu options'ı program.cs'de dolduruyoruz ve bağlantımızı kuruyoruz.
+        }
+
+        //Ardından DbSet yani Tabloda karşılığı olacak table ismini yazıyoruz.
+
+        public DbSet<Product> Products { get; set; } //Products değil de başka bir isim vermek ister isem Product Entity'sine gelip başına [Table("Products")] yazıp mapleyebiliriz.
+
+        //Sonra Program.cs tarafına geliyoruz.
+    }
+}
